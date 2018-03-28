726207a747d00fb2faf834ab80a719292bc0da95
Mgen <m@mgenware.com>
2018-03-28 14:11:29 +0800

    Export both flexBuilder and flexBuilderOption

--- lib/main.ts
+++ lib/main.ts
@@ -1,24 +1,2 @@
-import { Builder } from '/Users/yuanyuanliu/layit';
-import FlexHandler from './flexHandler';
-import Option from './option';
-import log from './log';
-const xml = require('xml');
-
-export default class FlexBuilder extends Builder {
-  private opt: Option;
-
-  constructor(opt?: Option) {
-    opt = opt || new Option();
-    super(new FlexHandler(opt));
-    this.opt = opt;
-  }
-
-  build(element: Element): object {
-    const ret = super.build(element);
-    if (this.opt.logging) {
-      log('build', ret);
-    }
-    const xmlString = xml(ret);
-    return xmlString;
-  }
-}
+export { default as FlexBuilder } from './flexBuilder';
+export { default as FlexBuilderOption } from './option';

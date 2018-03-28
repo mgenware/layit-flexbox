68f488cf46cfe47d6de35eb38e71273b58da5d5a
Mgen <m@mgenware.com>
2018-03-28 14:11:34 +0800

    Enable logging in tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,8 +1,10 @@
-import FlexBuilder from '../lib/main';
+import { FlexBuilder, FlexBuilderOption} from '../lib/main';
 
 test('test add', () => {
   // <layit><h><h><View></View></h><v><h><View></View></h></v></h></layit>
-  const builder = new FlexBuilder();
+  const opt = new FlexBuilderOption();
+  opt.logging = true;
+  const builder = new FlexBuilder(opt);
   const element = FlexBuilder.elementFromXML('<layit><h><View></View></h></layit>');
 
   expect(builder.build(element)).toBe('<div></div>');

9a14d54357ae805b66cde45903132c72980a5910
Mgen <m@mgenware.com>
2018-04-02 15:35:23 +0800

    Update tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -8,5 +8,5 @@ test('test add', () => {
   const builder = new FlexBuilder(opt);
   const element = FlexBuilder.documentFromXML(src);
   const expected = builder.build(element);
-  expect(expected).toBe('<div></div>');
+  expect(expected).toBe('<div style="display:flex;flex:1 1 0;flex-direction:column;"><div style="background-color:yellow;flex:0 1 auto;">Header: auto</div><div style="display:flex;flex:1 1 0;flex-direction:row;"><div style="background-color:red;flex:0 1 100px;">Left bar: 100px</div><div style="background-color:green;flex:2 1 0;">Content section: *2</div><div style="background-color:blue;flex:1 1 0;">Right bar: *</div></div><div style="background-color:gray;flex:0 1 200px;">Footer: 200px</div></div>');
 });

f8ef6a822aa1bd151a3f9bfa97fc3f73212bbf07
Mgen <m@mgenware.com>
2018-04-06 12:01:19 +0800

    Make vh related tests pass

--- tests/vh.test.ts
+++ tests/vh.test.ts
@@ -1,13 +1,11 @@
 import { buildTestFile } from './base';
 
 test('v and h', async () => {
-  const result = await buildTestFile('vh.xml', {logging: true});
-  console.log(result);
+  const result = await buildTestFile('vh.xml');
   expect(result).toBe('<div style="display:flex;flex:1 1 0;flex-direction:column;"><div style="background-color:yellow;flex:0 1 auto;">Header: auto</div><div style="display:flex;flex:1 1 0;flex-direction:row;"><div style="background-color:red;flex:0 1 100px;">Left bar: 100px</div><div style="background-color:green;flex:2 1 0;">Content section: *2</div><div style="background-color:blue;flex:1 1 0;">Right bar: *</div></div><div style="background-color:gray;flex:0 1 200px;">Footer: 200px</div></div>');
 });
 
 test('vh with leaf text node', async () => {
   const result = await buildTestFile('vh-text.xml');
-  // console.log(result);
-  expect(result).toBe('<div style="display:flex;flex:1 1 0;flex-direction:column;"><div style="flex:1 1 0;"><div style="flex:1 1 0;">  h i </div></div><div style="flex:1 1 0;"> i </div></div>');
+  expect(result).toBe('<div style="display:flex;flex:1 1 0;flex-direction:column;"><div style="display:flex;flex:1 1 0;flex-direction:row;"><div style="display:flex;flex:1 1 0;flex-direction:column;">  h i </div></div><div style="display:flex;flex:1 1 0;flex-direction:column;"> i </div></div>');
 });

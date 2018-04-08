7c33328573a3d4b3ccf888156905136d28993261
Mgen <m@mgenware.com>
2018-04-09 02:23:25 +0800

    Update examples

--- example/main.js
+++ example/main.js
@@ -1,5 +1,5 @@
 const { FlexBuilder } = require('..');
 
 const builder = new FlexBuilder();
-const xml = `<layit><box h-align="stretch" style="background:green">ABC</box></layit>`;
+const xml = `<layit><box h-align="right" style="background:green" margin="20px">ABC</box></layit>`;
 console.log(builder.build(FlexBuilder.documentFromXML(xml)));
